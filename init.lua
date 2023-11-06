ad368c3f337eef8639feca86d6c9d685f05aff5e
Luan Luciano <luanluciano@outlook.com>
2023-11-05 21:18:04 -0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -6,9 +6,9 @@ Services = {
     --updater = "http://localhost/api/updater.php",
 }
 
-g_app.setName("OTClient - Redemption");
-g_app.setCompactName("otclient");
-g_app.setOrganizationName("otbr");
+g_app.setName("Holy Baiak");
+g_app.setCompactName("holybaiak");
+g_app.setOrganizationName("holy team");
 
 g_app.hasUpdater = function()
     return (Services.updater and Services.updater ~= "" and g_modules.getModule("updater"))
